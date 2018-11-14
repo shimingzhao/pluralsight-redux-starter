804e099e9e644a6482fb4d0d8daa026b5fa8de06
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2018-11-14 14:05:56 -0500

    async status and error handling

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,6 +1,6 @@
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
-// import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors){
   return {type:types.LOAD_AUTHORS_SUCCESS,authors};
@@ -8,7 +8,7 @@ export function loadAuthorsSuccess(authors){
 
 export function loadAuthors(){
   return dispatch =>{
-    // dispatch(beginAjaxCall());
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors().then(authors =>{
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error =>{
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,47 +1,53 @@
-import React, {PropTypes} from 'react'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import * as courseActions from '../../actions/courseActions'
 import CourseForm from './CourseForm'
 import toastr from 'toastr'
 
 class ManageCoursePage extends React.Component {
-  constructor(props, context) {
+  constructor (props, context) {
     super(props, context)
     this.state = {
       course: Object.assign({}, props.course),
       // authors: Object.assign({},this.props.authors),
-      // saving:false,
+      saving: false,
       errors: {}
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps (nextProps) {
     if (this.props.course.id != nextProps.course.id) {
       //Necessary to populate form when existing course in loaded directly.
-      this.setState({course: Object.assign({}, nextProps.course)});
+      this.setState({course: Object.assign({}, nextProps.course)})
     }
   }
 
-  updateCourseState(event) {
+  updateCourseState (event) {
     const field = event.target.name
     let courseUpdate = Object.assign({}, this.state.course)
     courseUpdate[field] = event.target.value
     return this.setState({course: courseUpdate})
   }
 
-  saveCourse(event) {
+  saveCourse (event) {
     event.preventDefault()
-    // this.setState({saving:true});
+    this.setState({saving: true})
     this.props.actions.saveCourse(this.state.course)
-    // .then(()=> this.redirect())
-    // .catch(error => {
-    //   toastr.error(error);
-    // });
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error)
+        this.setState({saving: false})
+      })
+  }
+
+  redirect () {
+    this.setState({saving: false})
+    toastr.success('Course saved')
     this.context.router.push('/courses')
   }
 
-  render() {
+  render () {
     return (
       <CourseForm
         allAuthors={this.props.authors}
@@ -49,6 +55,7 @@ class ManageCoursePage extends React.Component {
         onSave={(event) => this.saveCourse(event)}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
       />
     )
   }
@@ -65,14 +72,14 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 }
 
-function getCourseById(courses, id) {
+function getCourseById (courses, id) {
   const course = courses.filter(course => course.id == id)
   if (course.length)
     return course[0]
   return null
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps (state, ownProps) {
   const courseId = ownProps.params.id // from the path '/course/:id'
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
   if (courseId && state.courses.length > 0) {
@@ -91,7 +98,7 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps (dispatch) {
   return {
     actions: bindActionCreators(courseActions, dispatch)
   }
